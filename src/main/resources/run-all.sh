1f1e19679af3f6cc663834fda4205a481df75b69
Martin Biely <martin.biely@epfl.ch>
2013-09-13 11:36:44 +0200

    cluster/g5k unified scripts
    Conflicts:
    	src/main/resources/protocols.conf

--- src/main/resources/run-all.sh
+++ src/main/resources/run-all.sh
@@ -1,8 +1,8 @@
-
-
 #######
+: ${OAR_NODE_FILE:="${HOME}/nodelist"}
+: ${OAR_JOB_ID:=$$}
 #######
-JOB_ID=$OAR_JOB_ID
+JOB_ID=${OAR_JOB_ID}
 echo RUNNING JOB ${JOB_ID}
 echo starting at `date`
 
@@ -21,14 +21,14 @@ function do_killall ()
     killall -q java 2>&1 > /dev/null
     for node in ${NODES}; do
         echo "killing ${node}"
-        oarsh ${node} "bash -c \"killall -q java 2>&1 > /dev/null \"" 
+        oarsh ${node} "bash -c \"killall -q java 2>&1 > /dev/null \""
     done
 }
 
-function on_exit () 
+function on_exit ()
 {
     echo SHUTDOWN in PROGRESS
-    eval do_killall 
+    eval do_killall
     sleep 3
     echo END of RUN $JOB_ID
 }
@@ -36,29 +36,41 @@ function on_exit ()
 #trap on_exit EXIT
 do_killall
 
-(cd ${LOGDIR}; jar xf ${HOME}/@@DIR@@/jars/@@JAR@@ protocols.conf)
-#PROTOCONF=${LOGDIR}/protocols.conf
-# id=1
-# for node in ${NODES}; do
-#     echo "${id} lsr://${protoclass}@${node}:${port}/${protoid} " >> ${PROTOCONF}
-#     id=$((id+1))
-# done
+if [ -e ${HOME}/@@DIR@@/protocols.conf ]; then
+    cp ${HOME}/@@DIR@@/protocols.conf ${LOGDIR}
+fi
+
+if [ -e ${HOME}/@@DIR@@/properties ]; then
+    cp ${HOME}/@@DIR@@/properties ${LOGDIR}
+    echo running with properties:
+    cat ${HOME}/@@DIR@@/properties
+    X=`grep PROTOCOL= ${HOME}/@@DIR@@/properties`
+    if [ -n $X ]; then
+        eval $X
+        if [ -e ${HOME}/@@DIR@@/${PROTOCOL}.protocols.conf ]; then
+            cp ${HOME}/@@DIR@@/${PROTOCOL}.protocols.conf ${LOGDIR}/protocols.conf
+        else
+            echo "${PROTOCOL}.protocols.conf not found";
+            exit -1
+        fi
+    fi
+fi
 
 
 id=1
 for remote in ${NODES}; do
-    if [ ${remote} !=  ${LOCAL} ]; then 
-	echo starting @ ${remote}
-	oarsh ${remote} "@@DIR@@/run-one.sh ${id} ${LOGDIR}" &
+    if [ ${remote} !=  ${LOCAL} ]; then
+        echo starting @ ${remote}
+        oarsh ${remote} "@@DIR@@/run-one.sh ${id} ${LOGDIR}" &
     else
-	LOCAL_id=${id}
+        LOCAL_id=${id}
     fi
     id=$((id+1))
 done
 
 echo starting @ ${LOCAL}
 
-oarsh ${LOCAL} "@@DIR@@/run-one.sh ${LOCAL_id} ${LOGDIR}" 
+oarsh ${LOCAL} "@@DIR@@/run-one.sh ${LOCAL_id} ${LOGDIR}"
 
 echo finished locally at `date`
 sleep 3
--- src/main/resources/run-one.sh
+++ src/main/resources/run-one.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $# -lt 1 ]; then
-        echo "not enough arguments" 
+        echo "not enough arguments"
         exit 1
 fi
 
@@ -9,18 +9,23 @@ export LOG_DIR=$2
 OPTS="-server"
 LOCAL=`hostname`
 
+
 #OPTS="${OPTS} -agentlib:hprof=cpu=samples,interval=10,depth=5"
 
 CLASSPATH="${HOME}/@@DIR@@/:${HOME}/@@DIR@@/jars/*:"
 
+
 OPTS="${OPTS} -cp ${CLASSPATH}"
+# TODO switch protocol based on properties file
 OPTS="${OPTS} -Dprotocols.conf.url=file://${LOG_DIR}/protocols.conf "
 OPTS="${OPTS} -Dnodes.list.url=file://${LOG_DIR}/nodes "
-OPTS="${OPTS} -Dlsr.constants.file=file://${LOG_DIR}/constants.properties "
+OPTS="${OPTS} -Dlsr.constants.file=file://${LOG_DIR}/properties "
 
 COMMAND="java ${OPTS} ch.epfl.lsr.distal.deployment.DSLProtocolRunner ${LOCAL_ID}"
 
 (echo ID: ${LOCAL_ID} LOGDIR: ${LOG_DIR} HOST: ${LOCAL}
+    echo Starting at `date`
 echo running ${COMMAND}
 /usr/bin/time -v ${COMMAND}
-) >> ${LOG_DIR}/${LOCAL_ID}.log 2>&1
+echo Finished at `date`
+) >> ${LOG_DIR}/${LOCAL_ID}.log 2>${LOG_DIR}/${LOCAL_ID}.err.log
