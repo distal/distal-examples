83c6cbd57488c9ebd9fc0490d49019af1be932c3
Martin Biely <martin.biely@epfl.ch>
2014-03-11 12:14:53 +0100

    build -> running paxos

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
 import ch.epfl.lsr.sbt.distal.DistalLocalRunner._
+import ch.epfl.lsr.sbt.distal.G5kPlugin._
+
 
 object ExamplesBuild extends Build {
 
@@ -13,11 +15,11 @@ object ExamplesBuild extends Build {
        libraryDependencies ++= Seq(
 
          "org.apache.commons" % "commons-math" % "2.2",
-	 "ch.epfl.lsr" %% "distal" % "0.1"
+         "ch.epfl.lsr" %% "distal" % "0.1"
        ),
        resolvers ++= Seq(
-	 typesafe,
-	 typesafeSnapshot
+         typesafe,
+         typesafeSnapshot
        ),
       name := "distal-paxos",
       organization := "ch.epfl.lsr",
@@ -26,12 +28,17 @@ object ExamplesBuild extends Build {
       scalacOptions ++= Seq("-deprecation", "-feature"),
       fork := true
     ) ++ distalLocalRunnerSettings ++ Seq(
-      distalProtocolsMap := Map("1" -> Seq("ch.epfl.lsr.ping.Server"), "2" -> Seq("ch.epfl.lsr.ping.Client"))
-    ) ++ g5kSettings ++ Seq[Setting[_]](
+      distalProtocolsMap := Map(
+        "1" -> Seq("ch.epfl.lsr.paxos.ClassicClientStarter"),
+        "Replica1" -> Seq("ch.epfl.lsr.paxos.Server", "ch.epfl.lsr.paxos.PaxosServer"),
+        "Replica2" -> Seq("ch.epfl.lsr.paxos.Server", "ch.epfl.lsr.paxos.PaxosServer"),
+        "Replica3" -> Seq("ch.epfl.lsr.paxos.Server", "ch.epfl.lsr.paxos.PaxosServer"))
+        // distalProtocolsMap := Map("1" -> Seq("ch.epfl.lsr.ping.Server"), "2" -> Seq("ch.epfl.lsr.ping.Client"))
+    ) ++ g5kSettings ++ Seq(
         g5kSite := "luxembourg"
       , g5kCluster :=  "stremi"
       , g5kDst := "distal-paxos"
-      , g5kNodes := Seq(8)
+      , g5kNodes := Seq(8))
   }
 
   lazy val project = Project(id = "distal-examples",
